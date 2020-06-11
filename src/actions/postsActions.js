deacc44107971cb05852dd587799d3a3151bd24e
Joaquin Mayer <joakinmayer96@gmail.com>
2020-06-11 19:09:50 -0300

    Inmutabilidad

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { LOADING, ERROR, GET_POSTS_BY_USER } from "../types/postsTypes";
+import { GET_USERS } from "../types/usuariosTypes";
 
 export const getPostByUser = (key) => async (dispatch, getState) => {
   const { users } = getState().userReducer;
@@ -9,6 +10,18 @@ export const getPostByUser = (key) => async (dispatch, getState) => {
     `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`
   );
   const updated_posts = [...posts, res.data];
+  const posts_key = posts.length - 1;
+  const updated_users = [...users];
+  updated_users[key] = {
+    ...users[key],
+    posts_key,
+  };
+
+  dispatch({
+    type: GET_USERS,
+    payload: updated_users,
+  });
+
   dispatch({
     type: GET_POSTS_BY_USER,
     payload: updated_posts,
