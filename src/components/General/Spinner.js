9befd4f850c9b4566bcfe7ce02afaa01925f555d
Joaquin Mayer <joakinmayer96@gmail.com>
2020-06-10 22:25:03 -0300

    spinner

---
+++ src/components/General/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Spinner = (props) => (
+    <div className="center">
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+    </div>
+)
+
+export default Spinner;
--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,34 +1,42 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as userActions from "../../actions/usersActions";
+import Spinner from '../General/Spinner'
 
 function Users(props) {
   useEffect(() => {
     props.getAll();
-  }, [props]);
+  }, []);
 
-  return (
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Email</th>
-            <th>Enlace</th>
+  let ponerContenido = () => {
+    if(props.loading){
+      return <Spinner />
+    }
+
+    return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Email</th>
+          <th>Enlace</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.website}</td>
           </tr>
-        </thead>
-        <tbody>
-          {props.users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.website}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+        ))}
+      </tbody>
+    </table>
+  )};
+
+  return <div>
+    {ponerContenido()}
+  </div>;
 }
 
 const mapStatetoProps = (reducers) => {
